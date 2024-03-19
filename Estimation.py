f026f49ad642bad798349413f3121578e6d8ea08
Dana Elhajjar <163869626+elhajjad@users.noreply.github.com>
2024-03-19 09:47:15 +0100

    Create Estimation.py

---
+++ Estimation.py
@@ -0,0 +1,15 @@
+
+def SCM(X):  
+    """ A function that computes the ML Estimator for covariance matrix estimation for gaussian data
+        Inputs:
+            * 𝐗 = a matrix of size p*N with each observation along column dimension
+        Outputs:
+            * sigma_mle = the ML estimate"""
+    p = X.shape[0]
+    n = X.shape[1] 
+
+    # initialization
+    sigma_mle = np.zeros((p, p)) 
+  
+    sigma_mle = (X@X.conj().T) / n 
+return sigma_mle

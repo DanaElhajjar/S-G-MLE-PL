e70c033da2a52c23e833e0f2b8aeba6267c619c5
Dana Elhajjar <163869626+elhajjad@users.noreply.github.com>
2024-03-19 10:48:24 +0100

    Update Estimation.py

--- Estimation.py
+++ Estimation.py
@@ -20,3 +20,29 @@ def SCM(X):
   
     sigma_mle = (X@X.conj().T) / n 
 return sigma_mle
+
+def phasecorrection3(covmatrix): 
+    """
+    A function that corrects the phase of a covariance matrix to ensure phase alignment with the first element.
+        Inputs:
+            * covmatrix : Covariance matrix representing the phase information.
+        Outputs:
+            * phase_minus0 : corrected phases ensuring alignment with the phase of the first element. """
+    phase = -np.angle(covmatrix[0,:])
+    phase_minus0 = phase-phase[0]
+    return phase_minus0
+
+def phasecorrection4(covmatrix,sigmamatrix):
+    """    
+    A function that corrects the phase of a covariance matrix based on the sub-diagonal elements and sigma matrix.
+        Inputs:
+            * covmatrix : Covariance matrix 
+            * sigmamatrix : Coherence matrix
+        Outputs:
+        * phase : corrected phases based on sub-diagonal elements and coherence matrix. """
+    subdiagphase = np.zeros((sigmamatrix.shape[0]-1))
+    phase = np.zeros((sigmamatrix.shape[0]))
+    for i in range (sigmamatrix.shape[0]-1):
+        subdiagphase[i] = ((np.angle(covmatrix[i,i+1])) +np.pi)%(2*np.pi)-np.pi
+        phase[i+1] = (phase[i]+subdiagphase[i]+np.pi)%(2*np.pi)-np.pi
+    return -phase
